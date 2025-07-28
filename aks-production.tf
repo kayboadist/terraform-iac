1d29ff67c127b8b2d2f5bbb8e30c50e53fe2898e
kayboadist <kayboadist@yahoo.com>
2025-07-27 20:00:19 -0400

    Add initial AKS production module configuration

--- aks-production.tf
+++ aks-production.tf
@@ -1,10 +1,10 @@
-# module "aks-staging" {
+# module "aks-production" {
 #     source              = "./modules/aks"
 #     resource_group_name = var.resource_group_name
 #     location            = var.location
-#     cluster_name         = "${var.cluster_name}-staging"
-#     environment          = "staging"
-    
+#     cluster_name         = "${var.cluster_name}-production"
+#     environment          = "production"
+
 #     client_id     = var.client_id
 #     client_secret = var.client_secret
   
